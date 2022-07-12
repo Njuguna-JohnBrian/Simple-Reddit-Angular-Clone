347bb1b1c61fbbcfc9c77aef24dd68ddbed3d302
John Brian Ngugi Njuguna <njugunajb96@gmail.com>
2022-07-12 13:58:06 +0300

    Pass data down to child using input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
 
+import { Article } from './article/article.model';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angular-reddit';
+  articles: Article[];
+  constructor() {
+    this.articles = [
+      new Article('Angular', 'http://angular.io', 3),
+      new Article('FullStack', 'http://fullstack', 2),
+      new Article('Angular Homepage', 'http://angular.io', 1),
+    ];
+  }
 
   addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
     console.log(`Adding article title:${title.value} and link: ${link.value}`);
--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostBinding, OnInit } from '@angular/core';
+import { Component, HostBinding, Input, OnInit } from '@angular/core';
 import { Article } from './article.model';
 
 @Component({
@@ -8,12 +8,10 @@ import { Article } from './article.model';
 })
 export class ArticleComponent implements OnInit {
   @HostBinding('attr.class') cssClass = 'row';
+  @Input()
+  article!: Article;
 
-  article: Article;
-
-  constructor() {
-    this.article = new Article('Angular', 'http://angular.io', 10);
-  }
+  constructor() {}
 
   voteUp(): boolean {
     this.article.voteUp();
